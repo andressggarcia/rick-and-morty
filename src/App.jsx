5b2fc42c5d40475aaa6b219f2c83487397323a49
andressggarcia <andressggarcia2@gmail.com>
2022-11-06 18:48:58 -0300

    cambio de estilos en personajes

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import RickItem from './components/ResidentInfo';
+import frontPage from '../src/assets/image 3.png';
+import inFrontPage from '../src/assets/image 2.png';
+import RickLoad from "./assets/image_processing20210907-11935-193el2x.gif";
+
+import Pagiantion  from "./components/Pagiantion";
+
+
+function App() {
+
+  const[location, setLocation]= useState({})
+  const[inputId, setInputId]= useState("")
+  const [loading, setLoading]=useState(false);
+
+let info=0;
+  useEffect(() => {
+    const randomId = Math.floor(Math.random ()*126)
+    
+    axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
+      .then(res => setLocation(res.data))
+      /*---------Loading---------*/ 
+      setLoading(true)
+      
+setTimeout(()=>{
+  setLoading(false)
+},5000);
+},[])
+
+  console.log(location);
+  
+
+  const searchId = () => {
+    
+    axios.get(`https://rickandmortyapi.com/api/location/${inputId}`)
+      .then(res => setLocation(res.data))
+    
+  }
+
+
+
+  return (
+    
+    <>
+    {
+           loading ?
+           <div className='Loader'>
+            <img className='img' src={RickLoad} alt="" />
+           <h1 className='Loading'>Loading...</h1>
+             </div>
+           :
+
+    <div className="App" style={{background: "#242635"}}>
+      
+      <div className='banner'>
+        <img className='frontPage' src={frontPage} alt="" />
+        <div className='divFrontPage'>
+          <img className='inFrontPage' src={inFrontPage} alt="" />
+        </div>
+      </div>
+      
+      <div className='divSearch'>
+        <div className='search'>
+          <input className='input' type="text" value={inputId} placeholder="Escribe el nombre de la ubicación" onChange={e => setInputId(e.target.value)}/>
+          <button className=' seach-button' onClick={searchId}>Search</button>
+        </div>
+
+        <div className='divInfo'>
+          <div className='infoUno'><p>Nombre: <br /> <br /> {location.name} </p></div>
+          <div className='infoDos'><p>Dimensión: <br /> <br /> {location.dimension}</p></div>
+          <div className='infoTres'><p>Tipo: <br /> <br /> {location.type}</p></div>
+          <div className='infoCuatro'><p>Población: <br />  <br />{location.residents?.length} </p></div>
+        </div>
+        
+      
+
+         <div >
+            <ul className='charactersContainer'>
+              {location.residents?.map((residents) => (
+              
+                <RickItem key={residents} residents={residents}/>
+              ))}
+            </ul>
+           
+        </div>
+      </div>
+     
+     
+
+    </div>
+     }   
+     </>
+  )
+}
+
+export default App
---
+++ src/components/ResidentInfo.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
+const ResidentInfo = ({residents}) => {
+
+    const [characters, setCharacter] = useState({})
+
+
+    useEffect(() => {
+        axios.get(residents)
+            .then(res => setCharacter(res.data))
+    }, [])
+
+    console.log(characters);
+
+    return (
+        <>
+        <div className='divCharacters'>
+            <li >
+                <img className='image' src={characters.image} alt="" />
+                <div className='status'><p>{characters.status}</p></div>
+                <div className='infoCharacters'>
+                    <h3 className='nameCharacter'>{characters.name}</h3>
+                    <div className='hr'></div>
+                    <div>
+                        <p className='properties'>TYPE</p>
+                        <p className='features'>{characters.type}</p>
+                    </div>
+                    <div>
+                        <p className='properties'>ORIGIN</p>
+                        <p className='features'>{characters.origin?.name}</p>
+                    </div>
+                    <div>
+                        <p className='properties'>EPISODES WHERE APPEAR:</p>
+                        <p className='features'>{characters.episode?.length}</p>
+                    </div>
+                </div>
+            </li>
+           
+        </div>
+       
+        </>
+    );
+};
+
+export default ResidentInfo;
